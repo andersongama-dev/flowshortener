38672c2b8f9e8e35a80d440edc5291786da87d96
Anderson Gama <andersongamasilva5@gmail.com>
2026-08-06 16:20:24 -0300

    feat: add return JSON UrlController

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\UrlService;
 use Illuminate\Http\Request;
 
 class UrlController extends Controller
 {
-    public function shorten(Request $request) {
+    public function store(Request $request) {
         $request->validate(
             ["url"=>"required|url|max:2048"],
             ["url.required"=>"A URL é obrigatória"],
@@ -14,7 +15,14 @@ class UrlController extends Controller
             ["url.max"=>"A URL deve ter no máximo 2048 caracteres"]
         );
 
+        $urlService = new UrlService();
+        $url = $urlService->shorten($request->url);
         
+        return response()->json([
+            'id' => $url->id,
+            'original' => $url->original_url,
+            'shortened' => url('/' . $url->short_code)
+        ]);
 
     }
 }
--- app/Services/UrlService.php
+++ app/Services/UrlService.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Str;
 use App\Models\Url;
 
 class UrlService {
-    public function shorten(String $originalUrl):string {
+    public function shorten(String $originalUrl): Url {
         do{
             $code = Str::random(6);
         }while(Url::where("short_code", $code)->exists());
 
-        Url::create([
+        return Url::create([
             "original_url" => $originalUrl,
             "short_code" => $code
         ]);
